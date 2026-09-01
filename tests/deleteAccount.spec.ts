c14d0d0c4957e48dd99fd4136d945c47bc477ee1
rita <ritasecond427@gmail.com>
2026-09-01 08:07:24 +0800

    add test.step to login test

--- tests/deleteAccount.spec.ts
+++ tests/deleteAccount.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect } from "../utils/allure";
-import { HomePage } from '../pages/HomePage';
-import { SignInPage } from '../pages/SignInPage';
+import { HomePage } from "../pages/HomePage";
+import { SignInPage } from "../pages/SignInPage";
 
 const random = Math.floor(Math.random() * 1000000);
 const email = `testingninura${random}@gmail.com`;
@@ -9,28 +9,35 @@ test("Delete account", async ({ page }) => {
   const home = new HomePage(page);
   const signIn = new SignInPage(page);
 
-  await test.step('Akses website dan buka halaman Login', async () => {
+  await test.step("Open Website and open login page", async () => {
     await home.openWebsite();
     await home.clickLogin();
   });
 
-  await test.step(`Mulai proses pendaftaran dengan email: ${email}`, async () => {
+  await test.step(`Input name, email: ${email} and click signup button`, async () => {
     await signIn.signIn("Ninura", email);
-    await expect(page.locator("text=Enter Account Information")).toBeVisible(); 
+    await expect(page.locator("text=Enter Account Information")).toBeVisible();
   });
 
-  await test.step('Lengkapi formulir informasi akun dan data diri', async () => {
+  await test.step("Complete account information, personal details and click create account button", async () => {
     await signIn.infoPassword("5465765Qwert/-");
     await signIn.infoDays("22");
     await signIn.infoMonth("5");
     await signIn.infoYear("2005");
     await signIn.checkboxAccountInfo();
-    await signIn.info("Alexis", "Loh", "UCC", "Manggis Street NO. 45", "Papaya street No 90", "one");
+    await signIn.info(
+      "Alexis",
+      "Loh",
+      "UCC",
+      "Manggis Street NO. 45",
+      "Papaya street No 90",
+      "one",
+    );
     await signIn.infoCountry("Singapore");
     await signIn.info2("Waterway park", "675656", "6534215");
   });
 
-  await test.step('Hapus akun dan pastikan berhasil dihapus', async () => {
+  await test.step("Delete account and ensure it is deleted successfully", async () => {
     await home.deleteAccount();
     await expect(page.locator("text=Account Deleted!")).toBeVisible();
   });
--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -1,93 +1,103 @@
-import { test, expect} from "../utils/allure";
+import { test, expect } from "../utils/allure";
 
-import { HomePage } from '../pages/HomePage';
+import { HomePage } from "../pages/HomePage";
 
-import { LoginPage } from '../pages/LoginPage';
+import { LoginPage } from "../pages/LoginPage";
 
 test("Login with valid credential", async ({ page }) => {
-
   const home = new HomePage(page);
 
-const login=new LoginPage(page);
-
-await home.openWebsite();
-
-await home.clickLogin();
-
-await login.login("testingninura@gmail.com","5465765Qwert/-");
-
-await expect(page.locator("text=Logged in as")).toBeVisible();
-
+  const login = new LoginPage(page);
+  await test.step("Open Website and open login page", async () => {
+    await home.openWebsite();
+    await home.clickLogin();
+  });
+  await test.step("Input email, password and click login button", async () => {
+    await login.login("testingninura@gmail.com", "5465765Qwert/-");
+    await expect(page.locator("text=Logged in as")).toBeVisible();
+  });
+  await test.step("Verify successful login", async () => {
+    await expect(page.locator("text=Logged in as")).toBeVisible();
+  });
 });
 
 test("Login with unregistered email", async ({ page }) => {
   const home = new HomePage(page);
 
-const login=new LoginPage(page);
+  const login = new LoginPage(page);
+  await test.step("Open Website and open login page", async () => {
+    await home.openWebsite();
 
-await home.openWebsite();
-
-await home.clickLogin();
-
-await login.login("asdqwe34@gmail.com","5465765Qwert/-");
-
-await expect(page.locator("text=Your email or password is incorrect!")).toBeVisible();
-
+    await home.clickLogin();
+  });
+  await test.step("Input unregidtered email, password and click login button", async () => {
+    await login.login("asdqwe34@gmail.com", "5465765Qwert/-");
+  });
+  await test.step("Verify alert for email or password is incorrect appear", async () => {
+    await expect(
+      page.locator("text=Your email or password is incorrect!"),
+    ).toBeVisible();
+  });
 });
 
 test("Login with wrong password", async ({ page }) => {
   const home = new HomePage(page);
 
-const login=new LoginPage(page);
+  const login = new LoginPage(page);
+  await test.step("Open Website and open login page", async () => {
+    await home.openWebsite();
 
-await home.openWebsite();
-
-await home.clickLogin();
-
-await login.login("testingninura@gmail.com","12345678");
-
-await expect(page.locator("text=Your email or password is incorrect!")).toBeVisible();
-
+    await home.clickLogin();
+  });
+  await test.step("Input email, incorrect password and click login button", async () => {
+    await login.login("testingninura@gmail.com", "12345678");
+  });
+  await test.step("Verify alert for email or password is incorrect appear", async () => {
+    await expect(
+      page.locator("text=Your email or password is incorrect!"),
+    ).toBeVisible();
+  });
 });
 
 test("Login with invalid email", async ({ page }) => {
   const home = new HomePage(page);
+  const login = new LoginPage(page);
 
-const login=new LoginPage(page);
+  await test.step("Open Website and open login page", async () => {
+    await home.openWebsite();
+    await home.clickLogin();
+  });
+  await test.step("Input invalid email, password and click login button", async () => {
+    await login.login("testingninura", "5465765Qwert/-");
+  });
+  await test.step("Verify alert message appear", async () => {
+    const email = page.locator("[data-qa='login-email']");
 
-await home.openWebsite();
+    const validationMessage = await email.evaluate(
+      (el: HTMLInputElement) => el.validationMessage,
+    );
 
-await home.clickLogin();
-
-await login.login("testingninura","5465765Qwert/-");
-
- const email = page.locator("[data-qa='login-email']");
-
-  const validationMessage = await email.evaluate(
-    (el: HTMLInputElement) => el.validationMessage
-  );
-
-  expect(validationMessage).toContain("Please include an '@'");
-
+    expect(validationMessage).toContain("Please include an '@'");
+  });
 });
 
 test("Login without input email and password", async ({ page }) => {
   const home = new HomePage(page);
 
-const login=new LoginPage(page);
-
-await home.openWebsite();
-
-await home.clickLogin();
-
-await page.locator("[data-qa='login-button']").click();
-
- const loginEmail = page.locator("[data-qa='login-email']");
-
-  const message = await loginEmail.evaluate((el: HTMLInputElement) => {
-    return el.validationMessage;
+  const login = new LoginPage(page);
+  await test.step("Open Website and open login page", async () => {
+    await home.openWebsite();
+    await home.clickLogin();
   });
+  await test.step("Verify alert message appear", async () => {
+    await page.locator("[data-qa='login-button']").click();
 
-  expect(message).toBe('Please fill out this field.');
+    const loginEmail = page.locator("[data-qa='login-email']");
 
+    const message = await loginEmail.evaluate((el: HTMLInputElement) => {
+      return el.validationMessage;
+    });
+
+    expect(message).toBe("Please fill out this field.");
+  });
 });
